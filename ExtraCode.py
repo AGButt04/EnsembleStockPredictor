2146a3559cff68448bbd387baf1447cd36a5ecc1
Abdul Ghani Butt <agbutt@widener.edu>
2025-09-10 21:44:43 -0400

    Moved the previous features to extra code

--- ExtraCode.py
+++ ExtraCode.py
@@ -51,3 +51,21 @@
 # Checking how many null values are there and dropping them for our model
 # print("Missing Values: ")
 # print(appleData.isnull().sum())
+
+# Dropping Volume and Volume Yesterday.
+# features = ['Close', 'Volume', 'Daily_Return', 'Price_Yesterday',
+#           'Volume_Yesterday', 'MA_10', 'MA_50', 'Volatility']
+
+# Plotting feature importance
+# plt.figure(figsize=(8, 5))
+# plt.barh(
+#     feature_importance['features'],
+#     feature_importance['coefficient'],
+#     color='skyblue'
+# )
+# plt.axvline(x=0, color='red', linestyle='--')  # reference line at 0
+# plt.title("Feature Importance (Linear Regression Coefficients)")
+# plt.xlabel("Coefficient Value")
+# plt.ylabel("Feature")
+# plt.gca().invert_yaxis()  # largest on top
+# plt.show()
