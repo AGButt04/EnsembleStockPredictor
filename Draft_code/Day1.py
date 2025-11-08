aadfd0a50c282e82ad4bc0cc2dcc898e75c0935a
Abdul Ghani Butt <agbutt@widener.edu>
2025-11-08 17:53:44 -0500

    Renamed and Backtesting of the strategies practice

--- Draft_code/Day1.py
+++ Draft_code/Day1.py
@@ -1,10 +1,8 @@
 # Step 1: Import libraries
 import pandas as pd
-import tensorflow
 import yfinance as yf
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
